b3369f1567c316a8231e70e830e4cc2760fdb331
Claire McWhite <cmcwhite@login8.stampede.tacc.utexas.edu>
2016-12-15 16:42:05 -0600

    Correct typo

--- run_record.sh
+++ run_record.sh
@@ -2,7 +2,11 @@
 
 bash scripts/create_directory.sh $SCRATCH/metazoans/exp_list_example.txt
 
-#Here, make sure mzXMLS are in the experiment mzxml folder,  and the formatted fasta database in in the experimnet DB folder
+#Here, make sure mzXMLS are in the experiment mzxml folder,  and the formatted fasta database in in the experiment DB folder
+#exp_name
+#-----mzXML
+#-----DB
+
 
 
 bash scripts/create_commands.sh $SCRATCH/metazoans/exp_list_example.txt $WORK/MSblender/runMS2.sh $HOME/searchgui
