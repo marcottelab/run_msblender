b9574c2619bda0373855ac4d47717bf9b2479d19
clairemcwhite <claire.mcwhite@utexas.edu>
2017-06-19 14:44:15 -0500

    Add an example

--- run_record.sh
+++ run_record.sh
@@ -1,7 +1,6 @@
 
 
-bash scripts/create_directory.sh $SCRATCH/run_msblender/exp_list_example.txt
-
+bash scripts/create_directory.sh $SCRATCH/run_msblender/exp_list.txt
 #Here, make sure mzXMLS are in the experiment mzxml folder,  and the formatted fasta database in in the experiment DB folder
 #exp_name
 #-----mzXML
@@ -9,10 +8,10 @@ bash scripts/create_directory.sh $SCRATCH/run_msblender/exp_list_example.txt
 
 
 
-bash scripts/create_commands.sh $SCRATCH/run_msblender/exp_list_example.txt /work/03491/cmcwhite/MSblender/runMS2.sh $HOME/searchgui
+bash scripts/create_commands.sh $SCRATCH/run_msblender/exp_list_example.txt /work/03491/cmcwhite/MSblender/runMS2.sh $HOME/SearchGUI-3.2.18/
 
 
-bash scripts/create_sbatch.sh $SCRATCH/run_msblender/exp_list_example.txt
+bash scripts/create_sbatch.sh $SCRATCH/run_msblender/exp_list.txt
 
 
 
