d0242e9c2380cdd82815005688cb1295e548d769
clairemcwhite <claire.mcwhite@utexas.edu>
2017-08-24 15:44:39 -0500

    Add readme

--- run_record.sh
+++ run_record.sh
@@ -1,12 +1,8 @@
-bash scripts/create_directory.sh $SCRATCH/run_msblender/exp_list_example.txt
-#Here, make sure mzXMLS are in the experiment mzxml folder,  and the formatted fasta database in in the experiment DB folder
-#exp_name
-#-----mzXML
-#-----DB
+
+bash scripts/create_commands.sh exp_list.txt /work/03491/cmcwhite/MSblender/runMS2.sh /home1/03491/cmcwhite/SearchGUI-3.2.18/
 
 
+bash scripts/create_sbatch.sh exp_list.txt
 
-bash scripts/create_commands.sh $SCRATCH/run_msblender/exp_list_example.txt /work/03491/cmcwhite/MSblender/runMS2.sh $HOME/SearchGUI-3.2.18/
 
-
-bash scripts/create_sbatch.sh $SCRATCH/run_msblender/exp_list_example.txt
+grep "  " *COMMAN*
